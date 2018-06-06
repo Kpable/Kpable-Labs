2dd925f5afd37f86e4b3b6ba1d77bdfc855a7f16
lestherreynoso <lhreynos@gmail.com>
2018-06-05 22:46:09 -0400

    imported linked assets

---
+++ Assets/Tutorials/Alien Runner/Scripts/AbstractCharacter.cs
@@ -0,0 +1,130 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AbstractCharacter : MonoBehaviour {
+
+    [SerializeField]
+    private float m_MaxSpeed = 10f;                    // The fastest the player can travel in the x axis.
+    
+    [SerializeField]
+    private float m_JumpForce = 400f;                  // Amount of force added when the player jumps.
+    
+    [SerializeField]
+    private bool m_AirControl = false;                 // Whether or not a player can steer while jumping;
+    
+    [SerializeField]
+    private LayerMask m_WhatIsGround;                  // A mask determining what is ground to the character
+
+    private Transform m_GroundCheck;    // A position marking where to check if the player is grounded.
+    const float k_GroundedRadius = .4f; // Radius of the overlap circle to determine if grounded
+    private bool m_Grounded;            // Whether or not the player is grounded.
+    private Transform m_CeilingCheck;   // A position marking where to check for ceilings
+    const float k_CeilingRadius = .01f; // Radius of the overlap circle to determine if the player can stand up
+    private Animator m_Anim;            // Reference to the player's animator component.
+    private Rigidbody2D m_Rigidbody2D;
+    private bool m_FacingRight = true;  // For determining which way the player is currently facing.
+    private bool m_climbable = false;   //Whether the player is colliding with an object that is climbable
+
+    private void Awake()
+    {
+        // Setting up references.
+        m_GroundCheck = transform.Find("Bottom");
+        m_CeilingCheck = transform.Find("Top");
+        m_Anim = GetComponent<Animator>();
+        m_Rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        m_Grounded = false;
+
+        // The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
+        // This can be done using layers instead but Sample Assets will not overwrite your project settings.
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject != gameObject)
+                m_Grounded = true;
+        }
+        m_Anim.SetBool("Grounded", m_Grounded);
+
+        // Set the vertical animation
+        //m_Anim.SetFloat("vSpeed", m_Rigidbody2D.velocity.y);
+    }
+
+    public void Move(float hMove, float vMove, bool action, bool jump)
+    {
+        /*
+        // If crouching, check to see if the character can stand up
+        if (!crouch && m_Anim.GetBool("Crouch"))
+        {
+            // If the character has a ceiling preventing them from standing up, keep them crouching
+            if (Physics2D.OverlapCircle(m_CeilingCheck.position, k_CeilingRadius, m_WhatIsGround))
+            {
+                crouch = true;
+            }
+        }
+        */
+
+        // Set whether or not the character is crouching in the animator
+        //m_Anim.SetBool("Crouch", crouch);
+
+        //only control the player if grounded or airControl is turned on
+        if (m_Grounded || m_AirControl)
+        {
+            // Reduce the speed if crouching by the crouchSpeed multiplier
+            //move = (crouch ? move * m_CrouchSpeed : move);
+
+            // The Speed animator parameter is set to the absolute value of the horizontal input.
+            m_Anim.SetFloat("Speed", Mathf.Abs(hMove));
+
+            // Move the character
+            m_Rigidbody2D.velocity = new Vector2(hMove * m_MaxSpeed, m_Rigidbody2D.velocity.y);
+
+            // If the input is moving the player right and the player is facing left...
+            if (hMove > 0 && !m_FacingRight)
+            {
+                // ... flip the player.
+                Flip();
+            }
+            // Otherwise if the input is moving the player left and the player is facing right...
+            else if (hMove < 0 && m_FacingRight)
+            {
+                // ... flip the player.
+                Flip();
+            }
+        }
+        // If the player should jump...
+        if (m_Grounded && jump && m_Anim.GetBool("Grounded"))
+        {
+            // Add a vertical force to the player.
+            m_Grounded = false;
+            m_Anim.SetBool("Grounded", false);
+            m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
+        }
+        
+        //If grounded and player presses down
+        if (m_Grounded && vMove < 0)
+        {
+            m_Anim.SetBool("isDucking", true);
+            m_Rigidbody2D.velocity = Vector2.zero;
+        }
+        else
+        {
+            m_Anim.SetBool("isDucking", false);
+        }
+
+        if (action) { m_Anim.SetTrigger("action"); }
+    }
+
+    private void Flip()
+    {
+        // Switch the way the player is labelled as facing.
+        m_FacingRight = !m_FacingRight;
+
+        // Multiply the player's x local scale by -1.
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
---
+++ Assets/Tutorials/Alien Runner/Scripts/AbstractUserControl.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+[RequireComponent(typeof(AbstractCharacter))]
+public class AbstractUserControl : MonoBehaviour {
+
+    private AbstractCharacter m_Character;
+    private bool m_Jump, action;
+
+    private void Awake()
+    {
+        m_Character = GetComponent<AbstractCharacter>();
+    }
+
+
+    private void Update()
+    {
+        if (!m_Jump)
+        {
+            // Read the jump input in Update so button presses aren't missed.
+            m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
+        }
+
+        if (!action)
+        {
+            // Read the submit input in Update so button presses aren't missed.
+            action = CrossPlatformInputManager.GetButtonDown("Submit");
+        }
+    }
+
+
+    private void FixedUpdate()
+    {
+        float h = CrossPlatformInputManager.GetAxis("Horizontal");
+        float v = CrossPlatformInputManager.GetAxis("Vertical");
+        // Pass all parameters to the character control script.
+        m_Character.Move(h, v, action, m_Jump);
+        m_Jump = action = false;
+    }
+}
