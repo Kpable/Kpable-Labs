71bf8a4e41b0d7098b13a45dbc49c1f581b228db
lestherreynoso <lhreynos@gmail.com>
2018-06-24 12:01:22 -0400

    object pooling tutorial
    from brackeys [https://www.youtube.com/watch?v=tdSmKaJvCoA]

---
+++ Assets/Misc/Object Pools/Spawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+    ObjectPooler objectPooler;
+	// Use this for initialization
+	void Start () {
+        objectPooler = ObjectPooler.Instance;
+
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        objectPooler.SpawnFromPool("box", transform.position, Quaternion.identity);
+
+    }
+}
