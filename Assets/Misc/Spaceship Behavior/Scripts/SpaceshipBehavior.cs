1f49bc1f85d1b367e50ac48b305f52e6b69685f3
lestherreynoso <lhreynos@gmail.com>
2018-06-25 11:13:05 -0400

    Space ship behavior

---
+++ Assets/Misc/Spaceship Behavior/Scripts/SpaceshipBehavior.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceshipBehavior : MonoBehaviour {
+
+    public float maxSpeed = 10f;
+    public float rotationSpeed = 150f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Quaternion rot = transform.rotation;
+        float z = rot.eulerAngles.z;
+        z += -Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
+        rot = Quaternion.Euler(0, 0, z);
+        transform.rotation = rot;
+
+        Vector3 pos = transform.position;
+        Vector3 velocity = new Vector3(0, Input.GetAxis("Vertical") * maxSpeed * Time.deltaTime, 0);
+
+        pos += rot * velocity;
+
+        transform.position = pos;
+    }
+}
