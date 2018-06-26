513f14c570e1a4c3c37303982e1c5f7d1d53928a
lestherreynoso <lhreynos@gmail.com>
2018-06-26 14:08:14 -0400

    switched to rigid body2d for spaceship physics

---
+++ Assets/Misc/Spaceship Behavior/Scripts/NineGhost.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NineGhost : MonoBehaviour {
+
+    private Rect worldBounds;
+    private Sprite ghostSprite;
+
+	// Use this for initialization
+	void Start () {
+        worldBounds = GetComponent<WorldWrap>().worldBounds;
+        ghostSprite = GetComponent<SpriteRenderer>().sprite;
+
+        Vector3 topPosition = transform.position;
+        Vector3 bottomPosition = topPosition;
+
+        topPosition.y += worldBounds.height;
+        bottomPosition.y -= worldBounds.height;
+
+        GameObject ghost = new GameObject(name + "_Ghost");
+        ghost.AddComponent<SpriteRenderer>().sprite = ghostSprite;
+        ghost.transform.SetParent(transform);
+        ghost.transform.position = topPosition;
+
+        ghost = new GameObject(name + "_Ghost");
+        ghost.AddComponent<SpriteRenderer>().sprite = ghostSprite;
+        ghost.transform.SetParent(transform);
+        ghost.transform.position = bottomPosition;
+
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Misc/Spaceship Behavior/Scripts/SpaceshipBehavior.cs
+++ Assets/Misc/Spaceship Behavior/Scripts/SpaceshipBehavior.cs
@@ -4,27 +4,62 @@ using UnityEngine;
 
 public class SpaceshipBehavior : MonoBehaviour {
 
-    public float maxSpeed = 10f;
-    public float rotationSpeed = 150f;
+    public float acceleration = 15f;
+    public float maxSpeed = 250f;
+    public float rotationSpeed = 180f;
+    public float drag = 0.8f;
+
+    private float hAxis, vAxis;
+
+    Rigidbody2D body;
 
 	// Use this for initialization
 	void Start () {
-		
+        body = GetComponent<Rigidbody2D>();
+        if(body)
+            body.drag = drag;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Quaternion rot = transform.rotation;
-        float z = rot.eulerAngles.z;
-        z += -Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
-        rot = Quaternion.Euler(0, 0, z);
-        transform.rotation = rot;
-
-        Vector3 pos = transform.position;
-        Vector3 velocity = new Vector3(0, Input.GetAxis("Vertical") * maxSpeed * Time.deltaTime, 0);
-
-        pos += rot * velocity;
-
-        transform.position = pos;
+        hAxis = Input.GetAxis("Horizontal");
+        vAxis = Input.GetAxis("Vertical");
     }
+
+    private void FixedUpdate()
+    {
+
+        Rotation();
+        Thrust();
+    }
+
+    private void Rotation()
+    {
+        Quaternion currentRotation = transform.rotation;
+
+        float z = currentRotation.eulerAngles.z;
+        z += -hAxis * rotationSpeed * Time.deltaTime;
+
+        currentRotation = Quaternion.Euler(0, 0, z);
+
+        transform.rotation = currentRotation;
+    }
+
+    private void Thrust()
+    {
+        Vector3 velocity = new Vector3(0, vAxis * acceleration * Time.deltaTime, 0);
+
+        if (body)
+        {
+            body.AddForce(transform.rotation * velocity);
+
+        }
+        else
+        {
+            Vector3 pos = transform.position;
+            pos += transform.rotation * velocity;
+            transform.position = pos;
+        }
+    }
+
 }
