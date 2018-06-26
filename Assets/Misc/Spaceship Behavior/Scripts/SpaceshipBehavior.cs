5cd1f7c37ae8b87b1c3e019bdb53f52046d9fded
lestherreynoso <lhreynos@gmail.com>
2018-06-26 16:05:35 -0400

    merged develop into object pools

---
+++ Assets/Misc/Spaceship Behavior/Scripts/SpaceshipBehavior.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class SpaceshipBehavior : MonoBehaviour {
+
+    public float thrust = 250f;
+    public float rotationSpeed = 180f;
+    public float drag = 0.8f;
+    public float gravityScale = 0f;
+
+    private float hAxis, vAxis;
+
+    Rigidbody2D body;
+
+	// Use this for initialization
+	void Start () {
+        body = GetComponent<Rigidbody2D>();
+        body.drag = drag;
+        body.gravityScale = gravityScale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        hAxis = Input.GetAxis("Horizontal");
+        vAxis = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+
+        Rotation();
+        Thrust();
+    }
+
+    private void Rotation()
+    {
+        Quaternion currentRotation = transform.rotation;
+
+        float z = currentRotation.eulerAngles.z;
+        z += -hAxis * rotationSpeed * Time.deltaTime;
+
+        currentRotation = Quaternion.Euler(0, 0, z);
+
+        transform.rotation = currentRotation;
+    }
+
+    private void Thrust()
+    {
+        Vector3 force = new Vector3(0, vAxis * thrust * Time.deltaTime, 0);
+
+        body.AddForce(transform.rotation * force);
+
+    }
+
+}
---
+++ Assets/Misc/Spaceship Behavior/Scripts/WorldWrap.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldWrap : MonoBehaviour
+{
+
+    public Rect worldBounds = new Rect(-40, -40, 80, 80);
+    public bool enableNineGhost = true;
+
+    private Sprite ghostSprite;
+
+    private GameObject anchor;
+    private Transform[] ghostTransforms;
+
+    // Use this for initialization
+    void Start()
+    {
+        Sprite ghostSprite = GetComponent<SpriteRenderer>().sprite;
+
+        if (enableNineGhost)
+        {
+            ghostTransforms = new Transform[8];
+
+            anchor = new GameObject(name + "_Ghost_Anchor");
+
+            Vector3[] neighborhood = GetNeighborhood(transform.position, worldBounds);
+
+            for (int i = 0; i < neighborhood.Length; i++)
+            {
+                GameObject ghost = new GameObject(name + "_Ghost");
+                ghost.AddComponent<SpriteRenderer>().sprite = ghostSprite;
+                ghost.transform.SetParent(anchor.transform);
+                ghost.transform.localScale = transform.localScale;
+                ghost.transform.position = neighborhood[i];
+
+                ghostTransforms[i] = ghost.transform;
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(new Vector3(worldBounds.xMin, worldBounds.yMin), new Vector3(worldBounds.xMin, worldBounds.yMax));
+        Gizmos.DrawLine(new Vector3(worldBounds.xMin, worldBounds.yMin), new Vector3(worldBounds.xMax, worldBounds.yMin));
+        Gizmos.DrawLine(new Vector3(worldBounds.xMax, worldBounds.yMin), new Vector3(worldBounds.xMax, worldBounds.yMax));
+        Gizmos.DrawLine(new Vector3(worldBounds.xMin, worldBounds.yMax), new Vector3(worldBounds.xMax, worldBounds.yMax));
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Wrap();
+        if (enableNineGhost)
+            UpdateGhostsTransforms();
+    }
+
+    private void Wrap()
+    {
+        if (transform.position.x > worldBounds.xMax)
+            transform.position = new Vector3(worldBounds.xMin, transform.position.y);
+
+        if (transform.position.x < worldBounds.xMin)
+            transform.position = new Vector3(worldBounds.xMax, transform.position.y);
+
+        if (transform.position.y > worldBounds.yMax)
+            transform.position = new Vector3(transform.position.x, worldBounds.yMin);
+
+        if (transform.position.y < worldBounds.yMin)
+            transform.position = new Vector3(transform.position.x, worldBounds.yMax);
+    }
+
+    private void UpdateGhostsTransforms()
+    {
+        anchor.transform.position = transform.position;
+
+        for (int i = 0; i < ghostTransforms.Length; i++)
+        {
+            ghostTransforms[i].localRotation = transform.rotation;
+        }
+    }
+
+    Vector3[] GetNeighborhood(Vector3 position, Rect size)
+    {
+        Vector3[] hood = new Vector3[8];
+
+        // Top Left
+        hood[0] = new Vector3(position.x - size.width, position.y + size.height, 0);
+        // Top
+        hood[1] = new Vector3(position.x, position.y + size.height, 0);
+        // Top Right
+        hood[2] = new Vector3(position.x + size.width, position.y + size.height, 0);
+        // Left
+        hood[3] = new Vector3(position.x - size.width, position.y, 0);
+        // Right
+        hood[4] = new Vector3(position.x + size.width, position.y, 0);
+        // Bottom Left
+        hood[5] = new Vector3(position.x - size.width, position.y - size.height, 0);
+        // Bottom 
+        hood[6] = new Vector3(position.x, position.y - size.height, 0);
+        // Bottom Right
+        hood[7] = new Vector3(position.x + size.width, position.y - size.height, 0);
+
+        return hood;
+    }
+}
