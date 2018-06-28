1f4cd4a42cac0f1940732bec353c677a4459b5e6
lestherreynoso <lhreynos@gmail.com>
2018-06-27 23:20:06 -0400

    beginning weapon definition from tutorial

---
+++ Assets/Misc/Shmup Weapons/Projectile.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    [SerializeField]
+    private WeaponType _type;
+
+    public WeaponType type
+    {
+        get
+        {
+            return _type;
+        }
+        set
+        {
+            SetType(value);
+        }
+    }
+
+    void Awake()
+    {
+        
+    }
+
+    public void SetType(WeaponType eType)
+    {
+        _type = eType;
+        WeaponDefinition def = Main.GetWeaponDefinition(_type);
+        GetComponent<Renderer>().material.color = def.projectileColor;
+
+    }
+}
