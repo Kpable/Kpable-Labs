8c62c4bb60085a04f042a17a4f847a52b499c752
lestherreynoso <lhreynos@gmail.com>
2018-06-29 18:09:27 -0400

    Splitter behavior

--- Assets/Misc/Shmup Weapons/SplitterBehavior.cs
+++ Assets/Misc/Shmup Weapons/SplitterBehavior.cs
@@ -8,6 +8,7 @@ public class SplitterBehavior : MonoBehaviour {
     public int numberOfSplits = 3;  // number of times to split
     public int splitFactor = 2; // split by this each time
     public float sizeReduction = 0.5f;
+    public string poolTag;
 
     Health health;
     ObjectPooler objectPooler;
@@ -22,16 +23,19 @@ public class SplitterBehavior : MonoBehaviour {
 
     void Split()
     {
+        Debug.Log(name + " splitting into " + splitFactor + ", " + (sizeReduction * 100) + "% smaller versions");
         if (numberOfSplits > 0)
         {
             for (int i = 0; i < splitFactor; i++)
             {
-                GameObject split = objectPooler.SpawnFromPool("meteor", transform.position, Quaternion.identity);
+                GameObject split = objectPooler.SpawnFromPool(poolTag, transform.position, Quaternion.identity);
                 var sb = split.GetComponent<SplitterBehavior>();
 
                 sb.numberOfSplits = numberOfSplits - 1;
                 sb.splitFactor = splitFactor;
                 sb.sizeReduction = sizeReduction;
+
+                split.GetComponent<Health>().MaxHealth = Mathf.RoundToInt(health.MaxHealth * sizeReduction);
 
                 split.transform.localScale = transform.localScale * sizeReduction;
             }
--- Assets/Misc/Spaceship Behavior/Scripts/WorldWrap.cs
+++ Assets/Misc/Spaceship Behavior/Scripts/WorldWrap.cs
@@ -63,7 +63,7 @@ public class WorldWrap : MonoBehaviour
     void Update()
     {
         Wrap();
-        if (enableNineGhost)
+        if (enableNineGhost && anchor)
             UpdateGhostsTransforms();
     }
 
