e92b911fb76637eb6116c3cbf13618be1272e4af
lestherreynoso <lhreynos@gmail.com>
2019-01-16 23:18:22 -0500

    Update to the scriptable object steering design

--- Assets/Misc/Programming Game AI/LabController.cs
+++ Assets/Misc/Programming Game AI/LabController.cs
@@ -1,16 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Kpable.AI.Steering;
 
 public class LabController : MonoBehaviour {
 
-    public Steering[] agents;
+    public Vehicle[] agents;
     public Transform target;
 
 	// Use this for initialization
 	void Start () {
         agents[0].MaxSpeed = 10f;
         agents[0].MaxForce = 10f;
+        agents[0].MaxTurnRate = 2f;
         agents[0].Arrive(target.position);
 	}
 	
