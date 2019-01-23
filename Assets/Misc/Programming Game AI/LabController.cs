d23a52d8de46590fdaf8292ba2517841323bc3c4
lestherreynoso <lhreynos@gmail.com>
2019-01-22 21:46:17 -0500

    Updating scene to show multiple behaviors in action

--- Assets/Misc/Programming Game AI/LabController.cs
+++ Assets/Misc/Programming Game AI/LabController.cs
@@ -6,15 +6,20 @@ using Kpable.AI.Steering;
 public class LabController : MonoBehaviour {
 
     public Vehicle[] agents;
-    public Transform target;
+    public Transform[] targets;
 
 	// Use this for initialization
 	void Start () {
         agents[0].MaxSpeed = 10f;
         agents[0].MaxForce = 10f;
         agents[0].MaxTurnRate = 2f;
-        agents[0].Arrive(target.position);
-	}
+        agents[0].SetTarget(targets[0].position);
+
+        agents[1].MaxSpeed = 10f;
+        agents[1].MaxForce = 10f;
+        agents[1].MaxTurnRate = 2f;
+        agents[1].SetTarget(targets[1].position);
+    }
 	
 	// Update is called once per frame
 	void Update () {
