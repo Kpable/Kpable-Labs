012a711a1ad0c489e22cc38a7402c4ff4e313cc3
lestherreynoso <lhreynos@gmail.com>
2019-01-24 19:45:04 -0500

    Flee and pursuit scene work

--- Assets/Misc/Programming Game AI/LabController.cs
+++ Assets/Misc/Programming Game AI/LabController.cs
@@ -8,21 +8,37 @@ public class LabController : MonoBehaviour {
     public Vehicle[] agents;
     public Transform[] targets;
 
+    public enum TestBehavior { Seek, Arrive, Flee, Pursuit }
+
 	// Use this for initialization
 	void Start () {
-        agents[0].MaxSpeed = 10f;
-        agents[0].MaxForce = 10f;
-        agents[0].MaxTurnRate = 2f;
-        agents[0].SetTarget(targets[0].position);
+        agents[(int)TestBehavior.Seek].MaxSpeed = 10f;
+        agents[(int)TestBehavior.Seek].MaxForce = 10f;
+        agents[(int)TestBehavior.Seek].MaxTurnRate = 2f;
+        agents[(int)TestBehavior.Seek].SetTarget(targets[(int)TestBehavior.Seek].position);
 
-        agents[1].MaxSpeed = 10f;
-        agents[1].MaxForce = 10f;
-        agents[1].MaxTurnRate = 2f;
-        agents[1].SetTarget(targets[1].position);
+        agents[(int)TestBehavior.Arrive].MaxSpeed = 10f;
+        agents[(int)TestBehavior.Arrive].MaxForce = 10f;
+        agents[(int)TestBehavior.Arrive].MaxTurnRate = 2f;
+        agents[(int)TestBehavior.Arrive].SetTarget(targets[(int)TestBehavior.Arrive].position);
+
+        agents[(int)TestBehavior.Flee].MaxSpeed = 10f;
+        agents[(int)TestBehavior.Flee].MaxForce = 10f;
+        agents[(int)TestBehavior.Flee].MaxTurnRate = 2f;
+        agents[(int)TestBehavior.Flee].SetTarget(targets[(int)TestBehavior.Flee].position);
+
+        agents[(int)TestBehavior.Pursuit].MaxSpeed = 10f;
+        agents[(int)TestBehavior.Pursuit].MaxForce = 10f;
+        agents[(int)TestBehavior.Pursuit].MaxTurnRate = 2f;
+        agents[(int)TestBehavior.Pursuit].SetTargetVehicle(targets[(int)TestBehavior.Pursuit].GetComponent<Vehicle>());
     }
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void FixedUpdate () {
+        agents[(int)TestBehavior.Seek].SetTarget(targets[(int)TestBehavior.Seek].position);
+        agents[(int)TestBehavior.Arrive].SetTarget(targets[(int)TestBehavior.Arrive].position);
+        agents[(int)TestBehavior.Flee].SetTarget(targets[(int)TestBehavior.Flee].position);
+        //agents[(int)TestBehavior.Pursuit].SetTarget(targets[(int)TestBehavior.Pursuit].position);
+
+    }
 }
